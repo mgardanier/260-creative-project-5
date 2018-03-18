072d74f0818874f00a2589049f04ecf688b9e305
root <root@localhost.localdomain>
2018-03-17 21:00:59 -0600

    cors fix.

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+var cors = require('cors');
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('dist'))
+app.use(cors());
 
 let players = [];
 let id = 0;
